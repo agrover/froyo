d7eae157b3c8280158993ca2346271a8cd0229c1
Andy Grover <agrover@redhat.com>
2015-10-23 11:45:20 -0700

    Flesh out cmdline options
    Cover the standard bases, add, remove, create, list, status.
    
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate devicemapper;
 extern crate clap;
 
 use std::io::Result;
+use std::error::Error;
+use std::process::exit;
 
 use devicemapper as devm;
 use clap::{App, Arg, SubCommand, ArgMatches};
@@ -23,27 +25,69 @@ fn main() {
         .version(&crate_version!())
         .author("Andy Grover <andy@groveronline.com>")
         .about("Drobo + Free + YOLO")
+        .arg(Arg::with_name("debug")
+             .short("d")
+             .help("Print additional output for debugging"))
         .subcommand(SubCommand::with_name("list")
-                    .about("lists stuff")
-                    .arg(Arg::with_name("poolname")
-                         .help("optional pool to list")
+                    .about("List all froyodevs")
+                    .arg(Arg::with_name("long")
+                         .short("l")
+                         .help("Use a long listing format")))
+        .subcommand(SubCommand::with_name("status")
+                    .about("Get the status of a single froyodev")
+                    .arg(Arg::with_name("froyodevname")
+                         .required(true)
+                         .help("Froyodev to get info on")
                          .index(1)))
         .subcommand(SubCommand::with_name("add")
-                    .about("add a device to a pool")
-                    .arg(Arg::with_name("poolname")
-                         .help("pool to list")
+                    .about("Add one or more additional block devices to a froyodev")
+                    .arg(Arg::with_name("froyodevname")
+                         .help("Froyodev to add the device to")
+                         .required(true)
+                         .index(1))
+                    .arg(Arg::with_name("devices")
+                         .help("device to add")
+                         .multiple(true)
+                         .required(true)
+                         .index(2))
+                    )
+        .subcommand(SubCommand::with_name("remove")
+                    .about("Remove a block device from a froyodev")
+                    .arg(Arg::with_name("froyodevname")
+                         .help("Froyodev to remove the device from")
                          .required(true)
                          .index(1))
                     .arg(Arg::with_name("device")
-                         .help("device to add")
+                         .help("Block device to remove")
+                         .required(true)
+                         .index(2))
+                    )
+        .subcommand(SubCommand::with_name("create")
+                    .about("Create a new froyodev")
+                    .arg(Arg::with_name("froyodevname")
+                         .help("Name of the new froyodev")
+                         .required(true)
+                         .index(1))
+                    .arg(Arg::with_name("devices")
+                         .help("Initial block device(s) to use")
+                         .multiple(true)
                          .required(true)
                          .index(2))
                     )
         .get_matches();
 
-    match matches.subcommand() {
+    let r = match matches.subcommand() {
         ("list", Some(matches)) => list(matches),
         ("add", Some(matches)) => add(matches),
+        ("", None) => {
+            println!("No command given, try \"help\"");
+            Ok(())
+        }
         _ => unreachable!(),
+    };
+
+    if let Err(r) = r {
+        println!("there was an error: {}", r.description());
+        exit(1);
     };
 }
